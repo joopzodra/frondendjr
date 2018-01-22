b353664e78c4a78ebd1feeb68e20bdfeb51e65b8
Joop Rosier <joop@zodra.nl>
2018-01-22 18:33:43 +0100

    deleted lines for cors, they were not used anymore

--- src/routes/goodreads-router.js
+++ src/routes/goodreads-router.js
@@ -1,27 +1,6 @@
 const express = require('express');
-const cors = require('cors');
 const request = require('request');
 const goodreads = express.Router();
-
-// CORS async, see https://www.npmjs.com/package/cors
-/*var whitelist = ['http://frontendjr.nl', 'https://frontend.jr.nl, http://localhost:8000'];
-var corsOptionsDelegate = function(req, callback) {
-  var corsOptions;
-  if (whitelist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = {
-      origin: true
-    };
-  } else {
-    corsOptions = {
-      origin: false
-    };
-  }
-  callback(null, corsOptions);
-};
-
-goodreads.use(cors(corsOptionsDelegate), function(req, res, next) {
-  next();
-});*/
 
 goodreads.get('/api/search/*', function(req, res, next) {
   var searchString = req.url.substr(12);

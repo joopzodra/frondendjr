edcc368cbbf4fa01de05c484482a1ac0a49e5f98
Joop Rosier <joop@zodra.nl>
2018-01-30 19:40:38 +0100

    correction in session setup

--- src/routes/gedichtenDb-router/gedichtenDb-router.js
+++ src/routes/gedichtenDb-router/gedichtenDb-router.js
@@ -19,26 +19,30 @@ const seqStore = new SequelizeStore({
 //seqStore.sync(); // Only when using a new DB. Sequelize sync method syncs all defined models to the DB. In this case a new table Session will be added to the DB.
 //User.sync();
 
-gedichtenDb.use(bodyParser.json());
-gedichtenDb.use(session({
+const sess = {
   secret: 'tkKKLOKKD(*&^KI*ue74', //TODO: via environment var
   store: seqStore,
   resave: false,
-  saveUninitialized: false
-}));
+  saveUninitialized: false,
+  cookie: {}
+};
 
 // for express-session if app is behind reverse proxy and using ssl (see https://www.npmjs.com/package/express-session#cookiesecure)
 gedichtenDb.use(function(req, res, next) {
   if (process.env.NODE_ENV === 'production') {
     req.app.set('trust proxy', 1);
-    session.cookie = {
-      secure: true
-    };
+    sess.cookie.secure = true;
   }
-  //console.log('protocol:', req.protocol); nginx is configured with: proxy_set_header X-Forwarded-Proto $scheme; So the req.protocol should be https
+  //console.log('protocol:', req.protocol); //nginx is configured with: proxy_set_header X-Forwarded-Proto $scheme; So the req.protocol should be https
   next();
 });
 
+gedichtenDb.use(bodyParser.json());
+gedichtenDb.use(session(sess));
+gedichtenDb.use(function(req, res, next) {
+  console.log(req.session, req.sessionID)
+  next();
+})
 gedichtenDb.use(passport.initialize());
 gedichtenDb.use(passport.session());
 setupPassport();

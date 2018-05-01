4410df98d3e35675c4f0721e3cc11f2f549ea185
Joop Rosier <joop@zodra.nl>
2018-05-01 10:51:17 +0200

    gedichtenDb: corrections in image handling

--- src/routes/gedichtenDb-router/gedichtenDb-manager.js
+++ src/routes/gedichtenDb-router/gedichtenDb-manager.js
@@ -25,19 +25,20 @@ function ensureAuthenticated(req, res, next) {
   }
 }
 
-function setPoetImgFileName(id, item, file, affectedCount) {
-  // If id, then it's a new item, and POST/create returns an item, else it's an updated item and PUT/update return the number of affected database-table-rows (as a number in an array). Item is needed in frontend; affectedCount is only for dev check.
-  const resolveValue = id ? affectedCount : item;
+function renamePoetImg(imgUrl, item, file, affectedCount) {
+  // If item, it's a new item, and POST/create returns an item. Else, it's an updated item and PUT/update return the number of affected database-table-rows (as a number in an array). Item is needed in frontend; affectedCount is only for dev check.
+  const resolveValue = item ? item: affectedCount;
   if (!file) {
     return Promise.resolve(resolveValue);
   }
-  const itemId = id ? id : item.id;
   const fileExt = getFileExt(file.mimetype);
+  const url = imgUrl ? imgUrl : item.item_id + fileExt;
   const fileName = file.filename;
-  return new Promise((resolve, reject) => {
-    fs.rename(path.join(uploadDir, fileName), path.join(uploadDir, itemId + fileExt), (err) => {
+  return new Promise((resolve) => {
+    fs.rename(path.join(uploadDir, fileName), path.join(uploadDir, url), (err) => {
       if (err) {
-        reject(err);
+        console.log(err);
+        resolve(resolveValue);
       } else {
         resolve(resolveValue);
       }
@@ -56,16 +57,25 @@ function getFileExt(mimetype) {
   return fileExt;
 }
 
-function deletePoetImg(id, imgUrl, tableRows) {
+function checkFile(req) {
+  if (!(req.file.mimetype === 'image/jpeg' || req.file.mimetype === 'image/png') || req.file.size > 24576) {
+    deletePoetImg(req.file.filename, undefined);
+    req.file = undefined;
+  }
+}
+
+function deletePoetImg(imgUrl, tableRows) {
   // tableRows is the return value of Poet.delete; we just pass it on.
   if (!imgUrl) {
     return Promise.resolve(tableRows);
   }
   const filePath = path.join(uploadDir, imgUrl);
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     fs.unlink(filePath, err => {
       if (err) {
-        reject(err);
+        console.log(err);
+        resolve(tableRows);
+        //reject(err);
       } else {
         resolve(tableRows);
       }
@@ -173,6 +183,7 @@ gedichtenDbManager.get('/find-all', ensureAuthenticated, (req, res, next) => {
     .catch(err => next(err));
   });
 
+  // If the formdata contains an image, it is bind to req.file. This is done by the multer package. The function upload.single() is a multer function.
   gedichtenDbManager.post('/create', ensureAuthenticated, upload.single('img'), (req, res, next) => {
     const userId = req.user.id || 0;
     const table =  arrayWrap(req.query.table || '')[0];
@@ -201,9 +212,17 @@ gedichtenDbManager.get('/find-all', ensureAuthenticated, (req, res, next) => {
             died: json.died,
             user_id: userId,
             img_url: imgExt ? imgExt : ''
-          })
-          //.then(res => {console.log('created res: ', res); return res;})
-          ;          
+          });          
+        })
+        .then(createdItem => renamePoetImg(undefined, createdItem, req.file))
+        .then(createdItem => {
+          if (req.file) {
+            const imgUrl = createdItem.getDataValue('item_id') + imgExt;
+            createdItem.set('img_url', imgUrl);
+            return createdItem.save();            
+          } else {
+            return createdItem;
+          }
         });
         case 'bundles':
         return Bundle.max('id').then(maxId => {
@@ -219,17 +238,29 @@ gedichtenDbManager.get('/find-all', ensureAuthenticated, (req, res, next) => {
       }
     };
     createItem()
-    .then(createdItem => setPoetImgFileName(undefined, createdItem, req.file))
     .then(createdItem => res.json(createdItem))
     .catch(err => next(err));
   });
 
+  // If the formdata contains an image, it is bind to req.file. This is done by the multer package. The function upload.single() is a multer function.
   gedichtenDbManager.put('/update', ensureAuthenticated, upload.single('img'), (req, res, next) => {
     const userId = req.user.id || 0;
     const table =  arrayWrap(req.query.table || '')[0];
     const json = req.body;
+    if (req.file) {
+      checkFile(req);
+    }
     const imgExt = req.file ? getFileExt(req.file.mimetype) : null;
-    const imgUrl = req.file ? json.id + imgExt : json.img_url; 
+    // Note: json.img_url is current url. If we get a new file, we must update the filename, otherwise the browser cache will still show the previous image. If we don't get a file, we store the existing url again in the database.
+    let imgUrl;
+    if (req.file) {
+      imgUrl = json.item_id + '-' + Date.now().toString() + imgExt;
+    } else if (json.delete_poet_img) {
+      imgUrl = '';
+    } else {
+      imgUrl = json.img_url;
+    }
+
     const updateItem = () => {
       switch(table) {
         case 'poems':
@@ -256,7 +287,15 @@ gedichtenDbManager.get('/find-all', ensureAuthenticated, (req, res, next) => {
           where: {
             [Op.and]: [{id: json.id}, {user_id: userId}]
           }
-        });
+        })
+        .then(affectedCount => {
+          if (req.file || json.delete_poet_img) {
+            return deletePoetImg(json.img_url)
+            .then(() => renamePoetImg(imgUrl, undefined, req.file, affectedCount));
+          }
+          return affectedCount;
+        })
+        .then(affectedCount => ({affectedCount: affectedCount, imgUrl: imgUrl}));
         case 'bundles':
         return Bundle.update({
           title: json.title,
@@ -270,8 +309,7 @@ gedichtenDbManager.get('/find-all', ensureAuthenticated, (req, res, next) => {
       }
     };
     updateItem()
-    .then(affectedCount => setPoetImgFileName(json.id, undefined, req.file, affectedCount))
-    .then(affectedCount => res.json({affectedCount: affectedCount, imgUrl: imgUrl}))
+    .then(result => res.json(result))
     .catch(err => next(err));
   });
 
@@ -294,7 +332,7 @@ gedichtenDbManager.get('/find-all', ensureAuthenticated, (req, res, next) => {
             [Op.and]: [{id: id}, {user_id: userId}]
           }
         })
-        .then(tableRows => deletePoetImg(id, imgUrl, tableRows));
+        .then(tableRows => deletePoetImg(imgUrl, tableRows));
         case 'bundles':
         return Bundle.destroy({
           where: {
--- src/routes/gedichtenDb-router/user-data-helpers.js
+++ src/routes/gedichtenDb-router/user-data-helpers.js
@@ -9,13 +9,12 @@ const uploadDir = path.join(__dirname, '../../public/uploads/gedichtenDb');
 module.exports = {
   insertUserData: (userId) => {
 
+    setTimeout(deleteUserData, 86400000 + 60000); //one day plus one minute
+
     const adminUserId = 49;
     const queryPoems = `INSERT INTO poems (title, text, text_lines, poet_id, bundle_id, url, url_label, comment, user_id, createdAt, updatedAt) SELECT title, text, ${0}, poet_id, bundle_id, url, url_label, comment, ${userId}, datetime('now'), datetime('now') FROM poems WHERE user_id=${adminUserId}`;
     const queryPoets = `INSERT INTO poets SELECT id, name, born, died, ${userId}, null, datetime('now'), datetime('now'), img_url FROM poets WHERE user_id=${adminUserId}`;
     const queryBundles = `INSERT INTO bundles SELECT id, title, poet_id, year, ${userId}, null, datetime('now'), datetime('now') FROM bundles WHERE user_id=${adminUserId}`;
-
-    //const updateImgUrl = (item_id, img_url) => item_id + '.' + img_url.split('.').pop();
-    //const updatePoets = `UPDATE poets SET img_url=poets.item_id+poets.img_url WHERE user_id=${userId}`;
 
     return sequelize.transaction(function(t) {
       return Promise.all([
@@ -36,12 +35,14 @@ module.exports = {
     .then(() => {
       return Poet.findAll({where: {user_id : userId}})
       .then(poets => {
+        // First promise: add img_url to poets, based on item_id
         const prom1 = Promise.all(poets.map(poet => {
           const itemId = poet.getDataValue('item_id');
           const imgExt = '.' + poet.getDataValue('img_url').split('.').pop();
           poet.set('img_url', itemId + imgExt);
           return poet.save();          
         }));
+        // Second promise: copy poets images and give them names based on item_id
         const prom2 = Promise.all(poets.map(poet => {
           return new Promise((resolve, reject) => {
             const imgExt = '.' + poet.getDataValue('img_url').split('.').pop();
@@ -61,32 +62,32 @@ module.exports = {
     })
     ;
   },
+};
 
-  deleteUserData: () => {
-    const queryPoems = `DELETE FROM poems WHERE user_id!=49 AND createdAt < datetime('now', '-24 hours')`;
-    const queryPoets = `DELETE FROM poets WHERE user_id!=49 AND createdAt < datetime('now', '-24 hours')`;
-    const queryBundles = `DELETE FROM bundles WHERE user_id!=49 AND createdAt < datetime('now', '-24 hours')`;
-    const queryUsers = `DELETE FROM users WHERE id!=49 AND createdAt < datetime('now', '-24 hours')`;
+const deleteUserData = () => {
+  const queryPoems = `DELETE FROM poems WHERE user_id!=49 AND createdAt < datetime('now', '-24 hours')`;
+  const queryPoets = `DELETE FROM poets WHERE user_id!=49 AND createdAt < datetime('now', '-24 hours')`;
+  const queryBundles = `DELETE FROM bundles WHERE user_id!=49 AND createdAt < datetime('now', '-24 hours')`;
+  const queryUsers = `DELETE FROM users WHERE id!=49 AND createdAt < datetime('now', '-24 hours')`;
 
-    return sequelize.transaction(function(t) {
-      return Promise.all([
-        sequelize.query(queryPoems, {
-          type: sequelize.QueryTypes.DELETE,
-          transaction: t
-        }),
-        sequelize.query(queryPoets, {
-          type: sequelize.QueryTypes.DELETE,
-          transaction: t
-        }),
-        sequelize.query(queryBundles, {
-          type: sequelize.QueryTypes.DELETE,
-          transaction: t
-        }),
-        sequelize.query(queryUsers, {
-          type: sequelize.QueryTypes.DELETE,
-          transaction: t
-        })
-        ]);
-    });
-  }
+  return sequelize.transaction(function(t) {
+    return Promise.all([
+      sequelize.query(queryPoems, {
+        type: sequelize.QueryTypes.DELETE,
+        transaction: t
+      }),
+      sequelize.query(queryPoets, {
+        type: sequelize.QueryTypes.DELETE,
+        transaction: t
+      }),
+      sequelize.query(queryBundles, {
+        type: sequelize.QueryTypes.DELETE,
+        transaction: t
+      }),
+      sequelize.query(queryUsers, {
+        type: sequelize.QueryTypes.DELETE,
+        transaction: t
+      })
+      ]);
+  });
 };

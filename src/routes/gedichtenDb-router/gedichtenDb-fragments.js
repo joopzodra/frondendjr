f4543c73b0fab1a17afce8561958ca5942352ccb
Joop Rosier <joop@zodra.nl>
2018-06-04 22:02:39 +0200

    dagelijks-vers app scroll adjustment

--- src/routes/gedichtenDb-router/gedichtenDb-fragments.js
+++ src/routes/gedichtenDb-router/gedichtenDb-fragments.js
@@ -16,7 +16,7 @@ const emitter = new events.EventEmitter();
 
 // export an object for app.js, where the object becomes: const gedichtenDbFragments
 module.exports = {
-  cronJob: new cronJob('00,20,40 * * * * *', emitFragment),
+  cronJob: new cronJob('00,10,20,30,40,50 * * * * *', emitFragment),
   emitter: emitter,
   fragmentsRouter: gedichtenDbFragments 
 };

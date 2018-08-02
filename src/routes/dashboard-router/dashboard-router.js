95aee908e26719f17d23d74577fc53167ce0d982
Joop Rosier <joop@zodra.nl>
2018-08-02 19:53:44 +0200

    Dashboard router corrections in properties naming.

--- src/routes/dashboard-router/dashboard-router.js
+++ src/routes/dashboard-router/dashboard-router.js
@@ -1,8 +1,6 @@
 const express = require('express');
-const request = require('request-promise-native');
 const dashboard = express.Router();
 const arrayWrap = require('arraywrap');
-const parseString = require('xml2js').parseString;
 const path = require('path');
 const dbPath = path.join(__dirname, 'dashboard.db');
 const Sequelize = require('sequelize');
@@ -27,11 +25,11 @@ dashboard.get('/guardian-news', function(req, res, next) {
     body: includeBody === "true" ? item.fields.body : ''
   })))
   .then(items => res.json(items))
-  .catch(err => next(err))
+  .catch(err => next(err));
 });
 
 dashboard.get('/nos-news', function(req, res, next) {
-  const regex = RegExp('<p>(.*?)<\/p>')
+  const regex = RegExp('<p>(.*?)</p>');
   const pageSize = arrayWrap(req.query['page-size'] || '')[0];
   const includeBody = arrayWrap(req.query['include-body' || ''])[0];
   NosItem.findAll({limit: pageSize, attributes: ['data']})
@@ -43,7 +41,7 @@ dashboard.get('/nos-news', function(req, res, next) {
       trailText: regexResult ? regexResult[1] : '',
       thumbnail: item.enclosure ? item.enclosure[0].$.url : '',
       body: includeBody === "true" ? item.description[0] : ''
-    }
+    };
   }))
   .then(items => res.json(items))
   .catch(err => next(err));
@@ -54,9 +52,9 @@ dashboard.get('/openweathermap', function(req, res, next) {
   if (city === 'listcitynames') {
     OpenWeatherMapItem.findAll({attributes: ['current_weather']})
     .then(items => {
-      const cityNames = items.map(item => JSON.parse(item.current_weather).city)
-      res.json(cityNames)
-    })
+      const cityNames = items.map(item => JSON.parse(item.current_weather).city);
+      res.json(cityNames);
+    });
   } else if (city === 'all') {
     OpenWeatherMapItem.findAll({attributes:['city', 'current_weather', 'forecast']})
     .then(items => items.map(item => ({
@@ -73,13 +71,13 @@ dashboard.get('/openweathermap', function(req, res, next) {
         return     {
           current_weather: JSON.parse(item.current_weather),
           forecast: JSON.parse(item.forecast)
-        }      
+        };     
       }
     })
     .then(item => res.json(item))
-    .catch(err => next(err));    
+    .catch(err => next(err));
   }
-})
+});
 
 dashboard.get('/iex', function(req, res, next) {
   const widgetCompanies = ['AAPL', 'GOOG', 'FB'];
@@ -94,7 +92,7 @@ dashboard.get('/iex', function(req, res, next) {
         company: item.company,
         quote: parsedQuote.quote_data,
         day: JSON.parse(item.day)
-      }
+      };
     }))
     .then(items => res.json(items))
     .catch(err => next(err));
@@ -103,15 +101,15 @@ dashboard.get('/iex', function(req, res, next) {
     .then(items => items.map(item => {
       const parsedQuote = JSON.parse(item.quote);
       return {
-        companySymbol: parsedQuote.symbol,
+        company: item.company,
         quote: parsedQuote.quote_data,
         day: JSON.parse(item.day)
-      }
+      };
     }))
     .then(items => res.json(items))
     .catch(err => next(err));
   } else {
-    IexItem.findOne({attributes: ['company', 'month', 'two_years'], where: {symbol: companySymbol.toUpperCase()}})
+    IexItem.findOne({attributes: ['company', 'month', 'two_years'], where: {symbol: company.toUpperCase()}})
     .then(item => ({
       month: JSON.parse(item.month),
       twoYears: JSON.parse(item.two_years)
@@ -121,16 +119,11 @@ dashboard.get('/iex', function(req, res, next) {
   }
 });
 
-dashboard.use(function(req, res) {
-  res.status(404);
-  res.send('Pagina niet gevonden.');
-});
-
 dashboard.use(function(err, req, res, next) {
   console.log(err);
-  const message = 'Er is een probleem met de database.'
+  const message = 'Er is een probleem met de database.';
   res.status(500);
   res.send(message);
-})
+});
 
 module.exports = dashboard;
